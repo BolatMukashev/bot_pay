b8f9ef2ba6020986f57d67b500271073b138ea7f
Bolat Mukashev <ya.ne.angel.kimi@gmail.com>
2021-02-23 17:01:47 +0500

    добавил картинку загрузки в promo_code

--- static/scripts/uppercase_script.js
+++ static/scripts/uppercase_script.js
@@ -1,6 +1,19 @@
+var loader__block = document.getElementById("loader__block");
+var secret_key = document.getElementById("secret_key");
+var promo_code = document.getElementById("promo_code");
+
+loader__block.style.display = 'none';
+
+// показать картинку загрузки при нажатии на кнопку, если поля promo_code и secret_key не пустые
+button.onclick = function() {
+    if (secret_key.value && promo_code.value) {
+        loader__block.style.display = 'flex';
+    }
+}
+
+// uppercase всех символов в поле promo_code при потере фокуса
 $(function() {
-    $('input').focusout(function() {
-        // Uppercase-ize contents
+    $('promo_code').focusout(function() {
         this.value = this.value.toLocaleUpperCase();
     });
 });
