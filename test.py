1b98d4be1fd5d17bc6492c68814a584a74467533
Bolat Mukashev <ya.ne.angel.kimi@gmail.com>
2021-10-31 13:08:07 +0500

    изменил шрифты на моноширинные

--- test.py
+++ test.py
@@ -131,6 +131,15 @@ def test_auto_schools():
 
 
 def create_poster(image_name, school_name, promo_code) -> None:
+    """
+    До 22 знака - размер шрифта 160, потом уменьшается
+    1025 пикселей - середина текста в постере
+    Использовать только моноширинные шрифты, у них символы одинаковой ширины (не сьзжают)
+    :param image_name: Название изображения
+    :param school_name: Название автошколы
+    :param promo_code: Промокод
+    :return:
+    """
     directory = os.path.join(os.getcwd(), 'static', 'images', image_name)
     directory2 = os.path.join(os.getcwd(), 'static', 'images', '2' + image_name)
 
@@ -140,15 +149,18 @@ def create_poster(image_name, school_name, promo_code) -> None:
         print('Файл изображения не найден')
     else:
         draw = ImageDraw.Draw(image)
-        font_dir = os.path.join(os.getcwd(), 'static', 'fonts', 'PT_Serif', 'PTSerif-Regular.ttf')
-        font = ImageFont.truetype(font_dir, size=160)
-        school_name_x_coord = 1025 - (len(school_name) / 2 * 110)
-        promo_code_x_coord = 1025 - (len(promo_code) / 2 * 90)
-        draw.text((school_name_x_coord, 200), "\"{}\"".format(school_name), (255, 255, 255), font=font)
-        draw.text((promo_code_x_coord, 580), promo_code, (255, 255, 255), font=font)
+        font_dir = os.path.join(os.getcwd(), 'static', 'fonts', 'Ubuntu_Mono', 'UbuntuMono-BoldItalic.ttf')
+
+        school_name_font_size = 160 if len(school_name) <= 22 else 160 - int((len(school_name) * 1.5))
+        promo_code_font_size = 160
+        school_name_font = ImageFont.truetype(font_dir, size=school_name_font_size)
+        promo_code_font = ImageFont.truetype(font_dir, size=promo_code_font_size)
+
+        draw.text((1025, 350), "\"{}\"".format(school_name), (255, 255, 255), anchor="ms", font=school_name_font)
+        draw.text((1025, 750), promo_code, (255, 255, 255), anchor="ms", font=promo_code_font)
         image.save(directory2, "PNG", optimize=True)
 
 
 if __name__ == '__main__':
-    create_poster('poster.png', 'BolAuto', 'Super_Hero28')
+    create_poster('poster.png', 'Академия Вождения в г.Уральск', 'BIG BOI 24 SUPER')
     # add_new_auto_school('BolAuto', 'KZ', 'Uralsk', [8777112255], ['m-bolat@mail.ru', 'ya.ne.angel.kimi@gmail.com'])
